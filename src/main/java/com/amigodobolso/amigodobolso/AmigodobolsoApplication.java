3b4a34a1ffcf3eb375909d1434b201e5306b8f7a
Junior Magalhães <jrlevita09@hotmail.com>
2023-02-19 16:52:35 -0300

    Estrutura do Projeto

---
+++ src/main/java/com/amigodobolso/amigodobolso/AmigodobolsoApplication.java
@@ -0,0 +1,13 @@
+package com.amigodobolso.amigodobolso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigodobolsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigodobolsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amigodobolso/amigodobolso/AmigodobolsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigodobolso.amigodobolso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigodobolsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
